71a2f517aeb9dc4c30549855ab4603b767556c50
Matt Auer <mattauer@umich.edu>
2024-03-18 18:02:59 -0500

    feat: get solflare wallet to sign messages and transfer

--- packages/wallet-connector-solana/src/SolflareSolanaWalletConnector.ts
+++ packages/wallet-connector-solana/src/SolflareSolanaWalletConnector.ts
@@ -77,7 +77,6 @@ export class SolflareSolanaWalletConnector extends FuelConnector {
       fuelProvider: 'https://beta-5.fuel.network/graphql',
       solanaProvider: new Solflare(),
     });
-    console.log(`window`, this.config.solanaProvider);
   }
 
   /**
@@ -213,121 +212,27 @@ export class SolflareSolanaWalletConnector extends FuelConnector {
       this.predicate.abi
     );
     predicate.connect(fuelProvider);
-    console.log(`transactionRequest.witnesses.length`, transactionRequest.witnesses.length);
-    //predicate.setData(transactionRequest.witnesses.length);
-    predicate.setData(0);
-
-    // Attach missing inputs (including estimated predicate gas usage) / outputs to the request
-    //await predicate.provider.estimateTxDependencies(transactionRequest);
-    // const resources = await predicate.getResourcesToSpend([{ amount: 1, assetId: BaseAssetId }]);
-    // transactionRequest.addPredicateResources(resources, predicate);
-    const reqClone = structuredClone(transactionRequest);
-    console.log(`transactionRequest`, reqClone);
-    console.log("pred addr", Address.fromString("fuel187g6jy2zzyemaqx2gfxcw5hyut3czk8vswurk98ux2jukue6wywskqxxxq").toHexString());
+    predicate.setData(transactionRequest.witnesses.length);
 
     // To each input of the request, attach the predicate and its data
     const requestWithPredicateAttached =
       predicate.populateTransactionPredicateData(transactionRequest);
 
     const txId = requestWithPredicateAttached.getTransactionId(chainId);
-    console.log(`txId`, txId);
     const u8TxId = arrayify(txId);
-    console.log(`arrayify(txID2)`, u8TxId);
-    const test = nacl.sign.detached(u8TxId, base58.decode("gvigzt9FYbKr3Y83LXPqLQaUQTecJj7ojhpawvfifiHutES51SHvLbkXfUFb6sBdgQDD5W1YH8ycqLhEVx5fWVQ"));
-    console.log(`test`, test);
-    console.log(`solanaProvider.signMessage`, solanaProvider.signMessage);
     const signedMessage = await solanaProvider.signMessage(u8TxId, "hex");
-    console.log(`signedMessage.signature`, signedMessage);
     const signature = hexlify(signedMessage);
-    console.log(`signature`, signature);
   
     // We have a witness, attach it to the transaction for inspection / recovery via the predicate
     // TODO: is below comment still relevant?
     // TODO: note that there is a strange witness before we add out compact signature
     //       it is [ 0x ] and we may need to update versions later if / when this is fixed
-    //transactionRequest.witnesses.push(signature);
-    transactionRequest.updateWitness(0, signature);
+    transactionRequest.witnesses.push(signature);
 
-    const transactionWithPredicateEstimated =
-      await fuelProvider.estimatePredicates(requestWithPredicateAttached);
-
-    console.log("obj", transactionWithPredicateEstimated)
-    console.log(`transactionWithPredicatesEstimated.toTransactionBytes()`, transactionWithPredicateEstimated.toTransactionBytes());
-    console.log(`transactionWithPredicatesEstimated.toTransactionBytes()`, hexlify(transactionWithPredicateEstimated.toTransactionBytes()));
-    const newClone = structuredClone(transactionRequest);
-    console.log(`newClone`, newClone);
     const response = await predicate.sendTransaction(transactionRequest);
-    // const temp  = await fuelProvider.simulate(transactionWithPredicateEstimated);
-    // console.log(`temp`, temp);
-    // const response = await fuelProvider.operations.submit({
-    //   encodedTransaction: hexlify(
-    //     transactionWithPredicateEstimated.toTransactionBytes()
-    //   ),
-    // });
-    console.log("done")
 
     return response.id;
-    //return response.submit.id;
   }
-
-  // async sendTransaction(
-  //   address: string,
-  //   transaction: TransactionRequestLike
-  // ): Promise<string> {
-  //   if (!(await this.isConnected())) {
-  //     throw new Error('No connected accounts');
-  //   }
-
-  //   const { solanaProvider, fuelProvider } = await this.getProviders();
-  //   const account = await this.getPredicateFromAddress(address);
-  //   if (!account) {
-  //     throw new Error(`No account found for ${address}`);
-  //   }
-  //   const transactionRequest = transactionRequestify(transaction);
-
-  //   const accountFund = Wallet.fromPrivateKey("0x80acb3fa5b95638671fe39747571ccd82f971da8bd26545542edb81c53848552", fuelProvider);
-  //   const script = new Script(this.predicate.bytecode, this.predicate.abi, accountFund);
-  //   await script.provider.estimateTxDependencies(transactionRequest);
-  //   console.log(`account.solanaAccount`, account.solanaAccount);
-  //   script.setConfigurableConstants({ SIGNER: account.solanaAccount });
-  //   //const tx = script.functions.main(transactionRequest.witnesses.length);
-  //   const tx = script.functions.main(1);
-  //   tx.txParams({ gasLimit: 100_000, gasPrice: 1 });
-  //   let txRequest = await tx.getTransactionRequest();
-  //   const chainId = await fuelProvider.getChainId();
-  //   const txId = await tx.getTransactionId(chainId);
-
-  //   await accountFund.fund(txRequest, [{ amount: bn(1), assetId: BaseAssetId }], bn(500));
-    
-  //   const txID2 = await txRequest.getTransactionId(chainId);
-  //   const txId2Clone1 = structuredClone(txID2);
-  //   console.log(`txID2`, txId2Clone1);
-  //   const u8TxId = arrayify(txID2);
-  //   console.log(`arrayify(txID2)`, u8TxId);
-  //   const test = nacl.sign.detached(u8TxId, base58.decode("gvigzt9FYbKr3Y83LXPqLQaUQTecJj7ojhpawvfifiHutES51SHvLbkXfUFb6sBdgQDD5W1YH8ycqLhEVx5fWVQ"));
-  //   console.log(`test`, test);
-  //   console.log(`solanaProvider.signMessage`, solanaProvider.signMessage);
-  //   const signedMessage = await solanaProvider.signMessage(u8TxId, "hex");
-  //   console.log(`signedMessage.signature`, signedMessage);
-  //   const signature = hexlify(signedMessage);
-  //   console.log(`signature`, signature);
-  //   txRequest.witnesses.push(signature);
-
-  //   await accountFund.populateTransactionWitnessesSignature(txRequest);
-
-  //   // const response = await fuelProvider.operations.dryRun({
-  //   //       encodedTransaction: hexlify(
-  //   //         txRequest.toTransactionBytes()
-  //   //       ),
-  //   // });
-  //   // console.log(`response`, response);
-  //   const response2 = await fuelProvider.sendTransaction(txRequest);
-  //   const res = await response2.waitForResult();
-  //   console.log(`response2`, res);
-
-  //   console.log("done")
-  //   return res.id!;
-  // }
 
   async currentAccount(): Promise<string | null> {
     if (!(await this.isConnected())) {
